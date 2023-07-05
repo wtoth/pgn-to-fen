3df7bb60c8683f51cf307538e356627fb589b0bd
Billy Toth <wtoth@umich.edu>
2023-07-05 18:42:08 -0400

    Scaffolding fen generation

---
+++ fen.py
@@ -0,0 +1,11 @@
+from generate_fen import generate_next_move
+
+class FEN:
+    def __init__(self, fen_file):
+        self.create_fen(fen_file)
+
+    metadata = []
+    game = []
+
+def create_fen(data):
+    pass
---
+++ generate_fen.py
@@ -0,0 +1,13 @@
+
+"""Generates the next FEN position from the previous FEN position and the next move.
+
+    Args:
+        previous_position (string): fen representation of the previous position
+        next_move (string): the next move in standard algebraic notation
+
+    Returns:
+        updated_position (string): fen representation of the updated position
+"""
+def generate_next_move(previous_position, next_move):
+    updated_position = ""
+    return updated_position
