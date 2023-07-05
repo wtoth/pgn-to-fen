9ad9a135c8979a68babc240978c4c7f93ce08cea
Billy Toth <wtoth@umich.edu>
2023-07-05 18:14:15 -0400

    updated pgn class to receive pgn data

--- pgn.py
+++ pgn.py
@@ -1,4 +1,7 @@
 class PGN:
+    def __init__(self, pgn_file):
+        self.create_pgn(pgn_file)
+
     pgn_dict = {
         "Event": "",
         "Site": "",
@@ -41,3 +44,19 @@ class PGN:
         "Termination": "",
         "Moves": []
     }
+
+    """Loads pgn data into the pgn object.
+
+    Args:
+        pgn_data (list): [metadata, pgn moves]
+
+    Returns:
+        (n/a)
+    """
+    def create_pgn(self, pgn_data):
+        self.pgn_dict["moves"] = pgn_data[1]
+        pgn_metadata = pgn_data[0].split('\n')
+        for line in pgn_metadata:
+            line = line.replace('[', '').replace(']', '').split(' ')
+            if line[0] in self.pgn_dict.keys():
+                self.pgn_dict[line[0]] = ' '.join(line[1:])
