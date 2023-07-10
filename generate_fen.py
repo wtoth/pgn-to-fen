f1cebac162aab7a6a3f27c4f8ba0cb4486d509b2
Billy Toth <wtoth@umich.edu>
2023-07-10 15:45:35 -0400

    Fen object now stores rank_file and fen notation

--- generate_fen.py
+++ generate_fen.py
@@ -1,6 +1,14 @@
-import re
 
+"""Initializes the .
 
+    Args:
+        previous_position (string): fen representation of the previous position
+        next_move (string): the next move in standard algebraic notation
+        turn (string): the color of the player whose turn it is (w or b)
+
+    Returns:
+        updated_position (string): fen representation of the updated position
+"""
 def generate_first_position(next_move):
     position = [["r", "n", "b", "q", "k", "b", "n", "r"],
                 ["p", "p", "p", "p", "p", "p", "p", "p"],
@@ -12,7 +20,6 @@ def generate_first_position(next_move):
                 ["R", "N", "B", "Q", "K", "B", "N", "R"]]
 
     return [position, next_move]
-
 
 """Generates the next FEN position from the previous FEN position and the next move.
 
@@ -28,42 +35,13 @@ def generate_next_move(previous_position, next_move, turn):
     updated_position = ""
     return updated_position
 
-
-"""Parses the pgn format to create a readable format for the generate_next_move function.
-
-    Args:
-        pgn_moves (string): string containing the pgn moves
-
-    Returns:
-        parsed_pgn (list[move, player (w or b), ]): fen representation of the updated position
-"""
-def pgn_format_parse(pgn_moves):
-    moves = []
-    split_by_move = re.split('\d+(\.) ', pgn_moves)
-    split_by_move = re.sub('[^0-9a-zA-Z]+', split_by_move)
-    for move in split_by_move:
-        move = move.split(' ')
-        if len(move) == 1:
-            moves.append([move[0], "w"])
-        else:
-            moves.append([move[0], "w"])
-            moves.append([move[1], "b"])
-    return moves
-
-"""Takes in the algebraic notation of a move and returns its current and new position.
+"""Takes in a rank file notation of a board and generates the fen representation.
 
     Args:
-        alg_notation (string): string containing the pgn moves
-        current_position (list[list[]]): multidimensional array representation of the current position
+        rank_file_notation (list[list[string]]): multidimensional list representation of the position
 
     Returns:
-        movement (list[current_position, new_position]): give coordinates of the starting position and the new position
+        fen (string): fen representation of the updated position
 """
-def algebraic_notation_to_rank_file(alg_notation, current_position, turn):
-    movement = []
-    if alg_notation == "o-o" or alg_notation == "O-O":
-        return ["castle short"]
-    elif alg_notation == "o-o-o" or alg_notation == "O-O-O":
-        return ["castle long"]
-    else:
-        pass
+def generate_fen_from_rank_file(rank_file_notation):
+    pass
