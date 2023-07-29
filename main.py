1418f145190e8314ac2feba3b0d6509f488ae6b5
wtoth <wtoth@umich.edu>
2023-07-29 16:17:29 -0400

    New main function for public view

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import import_pgn
+import fen
+import pgn
+
 def main():
+    Hikaru_raw_pgns = import_pgn.import_from_pgn_file("data/Nakamura_short.pgn")
+    Hikaru_pgns = []
+    for i in range(len(Hikaru_raw_pgns)):
+        pgn_game = pgn.PGN(Hikaru_raw_pgns[i])
+        Hikaru_pgns.append(pgn_game)
+    Hikaru_fens = []
+    for i in range(len(Hikaru_pgns)):
+        Hikaru_fens.append(fen.FEN(Hikaru_pgns[i]))
+
+
+def debug():
     pass
 
 if __name__ == "__main__":
