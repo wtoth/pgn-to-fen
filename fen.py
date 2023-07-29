ace11b083bd4e5a81ac0906879b53fdfb8c3a6df
wtoth <wtoth@umich.edu>
2023-07-29 16:17:02 -0400

    Updated pgn and fen constructors

--- fen.py
+++ fen.py
@@ -3,6 +3,7 @@ from notation_conversion import pgn_format_parse
 
 class FEN:
     def __init__(self, pgn):
+        self.game = []
         self.create_fen(pgn)
     game_id = None
     metadata = {}
@@ -13,15 +14,12 @@ class FEN:
     def create_fen(self, pgn):
         self.metadata = pgn.metadata
         self.game_id = pgn.game_id
-        #print(pgn.moves)
         parsed_pgn = pgn_format_parse(pgn.moves)
-        #print(parsed_pgn)
         first_move = True
         self.rank_file.append(generate_first_position(parsed_pgn[1]))
         self.game.append([generate_fen_from_rank_file(self.rank_file[-1]), parsed_pgn[0]])
 
         for i in range(len(parsed_pgn)):
-            #print(parsed_pgn[i])
             self.rank_file.append(generate_next_move(self.rank_file[-1][0], parsed_pgn[i]))
             if i + 1 < len(parsed_pgn):
                 self.game.append([generate_fen_from_rank_file(self.rank_file[-1]), parsed_pgn[i+1]])
--- pgn.py
+++ pgn.py
@@ -2,10 +2,24 @@ import uuid
 
 class PGN:
     def __init__(self, pgn_file):
+        self.game_id = uuid.uuid4()
         self.create_pgn(pgn_file)
 
-    game_id = uuid.uuid1()
-    metadata = {
+    game_id = uuid.uuid4()
+    metadata = {}
+    moves = []
+
+    """Loads pgn data into the pgn object.
+
+    Args:
+        pgn_data (list): [metadata, pgn moves]
+
+    Returns:
+        (n/a)
+    """
+    def create_pgn(self, pgn_data):
+        self.moves = pgn_data[1]
+        self.metadata = {
         "Event": "",
         "Site": "",
         "Date": "",
@@ -46,20 +60,9 @@ class PGN:
         "TimeControl": "",
         "Termination": "",
     }
-    moves = []
-
-    """Loads pgn data into the pgn object.
-
-    Args:
-        pgn_data (list): [metadata, pgn moves]
-
-    Returns:
-        (n/a)
-    """
-    def create_pgn(self, pgn_data):
-        self.moves = pgn_data[1]
         unprocessed_metadata = pgn_data[0].split('\n')
         for line in unprocessed_metadata:
             line = line.replace('[', '').replace(']', '').split(' ')
+            print(line)
             if line[0] in self.metadata.keys():
                 self.metadata[line[0]] = ' '.join(line[1:])
